6120cd058f8a154aab941d5cecf0a2ea9433c7f2
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2025-07-14 19:40:54 +0100

    add routing

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// import PDFRenderer from '../components/PDFConsentSigner.vue'
+import PDFSigner from '../components/PdfSigner.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <PDFRenderer /> -->
+    <PDFSigner />
+  </main>
+</template>
