1032cc6336353fa8bc4187f515157fcb03396976
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2025-07-14 19:41:08 +0100

    add pdf consent signer component

---
+++ src/components/PDFConsentSigner.vue
@@ -0,0 +1,19 @@
+<template>
+  <DocusealForm
+    :src="'https://docuseal.com/d/LEVGR9rhZYf86M'"
+    :email="'igbinobaebenezer@gmail.com'"
+  />
+</template>
+
+<script lang="ts">
+// ...
+import { DocusealForm } from '@docuseal/vue'
+
+export default {
+  name: 'App',
+  components: {
+    DocusealForm
+  },
+
+}
+</script>
