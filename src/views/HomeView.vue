6e099bd81e96b0d05d1940e0c2f79d6c2db137ca
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2025-07-23 03:27:51 +0100

    update home component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import DocusealBuilder from '../components/DocusealBuilder.vue'
 // import PDFRenderer from '../components/PDFConsentSigner.vue'
-import PDFSigner from '../components/PdfSigner.vue'
+// import PDFSigner from '../components/PdfSigner.vue'
 </script>
 
 <template>
   <main>
+    <DocusealBuilder></DocusealBuilder>
     <!-- <PDFRenderer /> -->
-    <PDFSigner />
+    <!-- <PDFSigner /> -->
   </main>
 </template>
