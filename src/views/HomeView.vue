93b943f1a37e0e47111276b575d3945664736562
Ebenezer Igbinoba <ebenezer.igbinoba@stu.cu.edu.ng>
2025-08-25 23:58:58 +0100

    update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import DocusealBuilder from '../components/DocusealBuilder.vue'
 // import PDFRenderer from '../components/PDFConsentSigner.vue'
-// import PDFSigner from '../components/PdfSigner.vue'
+import PDFSigner from '@/components/pdf-signer/PdfSigner.vue'
 </script>
 
 <template>
   <main>
-    <DocusealBuilder></DocusealBuilder>
+    <!-- <DocusealBuilder></DocusealBuilder> -->
     <!-- <PDFRenderer /> -->
     <!-- <PDFSigner /> -->
+     <PDFSigner />
   </main>
 </template>
