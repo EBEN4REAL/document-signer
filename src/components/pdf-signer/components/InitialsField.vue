dd0f8ee9b6a4ab573a6a31e7ba077066879678b7
Ebenezer Igbinoba <ebenezer.igbinoba@stu.cu.edu.ng>
2025-08-25 23:56:48 +0100

    add initials foeld component

---
+++ src/components/pdf-signer/components/InitialsField.vue
@@ -0,0 +1,30 @@
+<template>
+  <span class="initial-text">
+    {{ field.initialsText || 'Initials' }}
+  </span>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Field } from '@/types/index'
+
+defineProps<{
+  field: Field
+}>()
+</script>
+
+<style scoped>
+.initial-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1em;
+  color: #000;
+  font-weight: 600;
+  text-align: center;
+  width: 100%;
+  padding: 0 4px;
+  box-sizing: border-box;
+}
+</style>
